584351e505f7bcd2ab88519066a4e6a4fb999152
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2022-09-14 23:53:53 -0300

    Correção do componente tag

--- src/components/template/Projects/ProductionTag.tsx
+++ src/components/template/Projects/ProductionTag.tsx
@@ -2,7 +2,7 @@
 export function ProductionTag() {
   return (
     <div className="position absolute top-0 left-0 -rotate-45 z-[1] bg-[#fa0e0e] px-10 translate-y-[90%] translate-x-[-30%] font-extrabold rounded-lg text-sm">
-      debugging
+      building
     </div>
   );
 }
